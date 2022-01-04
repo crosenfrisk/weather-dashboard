5f80d3cabb834ea111754bdc1092730b386cdedb
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-01-04 09:21:33 -0600

    created readableDate for html so it isn't unix or a long string

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ var getWeather = function () {
 
   fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
-    //   console.log(data.name);
+      //   console.log(data.name);
     //   console.log(data);
 
       var divEl = document.querySelector("#Current-City");
@@ -34,7 +34,14 @@ var getWeather = function () {
       // Take input from user to return current city from API.
       currentCityEl.textContent = data.name;
 
-     // Take object property "temp" and display to html
+      // Display current date/time
+      var currentDateEl = document.querySelector("#current-date");
+      var dateObj = new Date(data.dt *1000);
+      var readableDate = dateObj.toLocaleDateString();
+    //   console.log(readableDate);
+      currentDateEl.textContent = readableDate;
+
+      // Take object property "temp" and display to html
       var tempEl = document.querySelector("#temp");
       tempEl.textContent = "Temperature: " + data.main.temp + " ° F";
 
@@ -42,11 +49,11 @@ var getWeather = function () {
       var humidityEl = document.querySelector("#humidity");
       humidityEl.textContent = "Humidity: " + data.main.humidity + " %";
 
-    // Take object property "wind speed" and display to html
-          var windEl = document.querySelector("#wind");
-          windEl.textContent = "Wind Speed: " + data.wind.speed + " mph";
+      // Take object property "wind speed" and display to html
+      var windEl = document.querySelector("#wind");
+      windEl.textContent = "Wind Speed: " + data.wind.speed + " MPH";
 
-    // Take object property "icon" and display to html
+      // Take object property "icon" and display to html
       var imgEl = document.createElement("img");
       imgEl.src =
         "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
@@ -56,26 +63,31 @@ var getWeather = function () {
 
       currentCityEl.appendChild(imgEl);
       currentCityEl.appendChild(iconEl);
+
+      // TODO: Add separate API for UV Index, then create function below for color/settings
     });
   });
 };
 
-var fiveDayForecast = function () {
-    // OpenWeather API for 5 Day forcast, formatted to fetch any city
-    var currentCityName = cityInputEl.value.trim();
-    var apiUrl5Day =
-      "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      currentCityName +
-      "&units=imperial&appid=24b908f651171bcc6920a65894cdfb4a";
-  
-    fetch(apiUrl5Day).then(function (response) {
-      response.json().then(function (data) {
-        console.log(data);
-      })
-    })
-    }
+// var fiveDayForecast = function () {
+//     console.log("testing");
+//   // OpenWeather API for 5 Day forecast, formatted to fetch any city
+//   var currentCityName = cityInputEl.value.trim();
+//   var apiFiveDay =
+//     "http://api.openweathermap.org/data/2.5/forecast?q=" +
+//     currentCityName +
+//     "&appid=24b908f651171bcc6920a65894cdfb4a";
 
-// Use lat/lon from weather endpoint to Onecall API for UV index.
+//   fetch(apiFiveDay).then(function (response) {
+//     response.json().then(function(data) {
+//       console.log(data);
+//     });
+//   });
+// }
+
+// fiveDayForecast();
+
+// Use lat/lon from "weather" endpoint to "onecall" API for UV index:
 
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
