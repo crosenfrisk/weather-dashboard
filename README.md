2cac929e6fc52fd91cfa17d0b972aca437cfa22e
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-01-04 11:48:33 -0600

    added UV Index from third Open Weather API

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather-dashboard
+
+https://coderrocketfuel.com/article/convert-a-unix-timestamp-to-a-date-in-vanilla-javascript
+https://gomakethings.com/how-to-use-the-fetch-method-to-make-multiple-api-calls-with-vanilla-javascript/
+https://openweathermap.org/forecast5
+https://openweathermap.org/api/one-call-api#parameter
+https://openweathermap.org/current#name
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#textfield");
+var currentCityName = cityInputEl.value.trim();
 
 // When a user types in a city in form input, API generates "Current City" on right side of screen; search also gets added to search history.
 
@@ -10,6 +11,8 @@ var formSubmitHandler = function (event) {
 
   if (currentCityName) {
     getWeather(currentCityName);
+    // uvIndex(lat, lon);
+    // fiveDayForecast(currentCityName);
     cityInputEl.value = "";
   } else if (!currentCityName === false) {
     alert("Please check the spelling of your city and try again.");
@@ -26,19 +29,20 @@ var getWeather = function () {
 
   fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
-      //   console.log(data.name);
-    //   console.log(data);
+      // console.log(data.name);
+      //  console.log(data);
 
       var divEl = document.querySelector("#Current-City");
       var currentCityEl = document.querySelector("#update-name");
       // Take input from user to return current city from API.
       currentCityEl.textContent = data.name;
 
-      // Display current date/time
+      // Display current date
       var currentDateEl = document.querySelector("#current-date");
-      var dateObj = new Date(data.dt *1000);
+      var dateObj = new Date(data.dt * 1000);
+      // Convert to JUST date, no time.
       var readableDate = dateObj.toLocaleDateString();
-    //   console.log(readableDate);
+      // Display converted date on html
       currentDateEl.textContent = readableDate;
 
       // Take object property "temp" and display to html
@@ -59,35 +63,79 @@ var getWeather = function () {
         "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
 
       var iconEl = document.querySelector("#icon");
-      icon.textContent = data.weather[0].icon;
+      iconEl.textContent = data.weather[0].icon;
+
+      var iconDescriptionEl = document.querySelector(".description");
+      iconDescriptionEl.textContent = data.weather[0].description;
 
       currentCityEl.appendChild(imgEl);
       currentCityEl.appendChild(iconEl);
+      currentDateEl.appendChild(iconDescriptionEl);
 
-      // TODO: Add separate API for UV Index, then create function below for color/settings
+      // Add API for UV Index using lat and lon from coord of previous API
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+
+      var apiUvIndexUrl =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&exclude=hourly,daily&appid=24b908f651171bcc6920a65894cdfb4a";
+
+      fetch(apiUvIndexUrl).then(function (response) {
+        response.json().then(function (data) {
+          // Display UV Index
+          var uviEl = document.querySelector("#uvi");
+          uviEl.textContent = "UV Index: " + data.current.uvi;
+
+          if (uviEl < 2) {
+            uviEl.textContent = "UV Index: " + data.current.uvi + "Favorable";
+            // Display color coded flag of UV Index to indicate favorable conditions
+            //  uviEl.appendChild(green);
+          } else if (uvi > 2 && uvi < 5) {
+            uviEl.textContent = "UV Index: " + data.current.uvi + "Moderate";
+            // Display color coded flag of UV Index to indicate moderate conditions
+            //  uviEl.appendChild(orange);
+          } else if (uvi > 5) {
+            uviEl.textContent = "UV Index: " + data.current.uvi + "Severe";
+            // Display color coded flag of UV Index to indicate severe conditions
+            //  uviEl.appendChild(red);
+          }
+        });
+      });
     });
   });
 };
 
-// var fiveDayForecast = function () {
-//     console.log("testing");
-//   // OpenWeather API for 5 Day forecast, formatted to fetch any city
-//   var currentCityName = cityInputEl.value.trim();
 //   var apiFiveDay =
 //     "http://api.openweathermap.org/data/2.5/forecast?q=" +
 //     currentCityName +
 //     "&appid=24b908f651171bcc6920a65894cdfb4a";
 
+//   // console.log(apiFiveDay);
 //   fetch(apiFiveDay).then(function (response) {
-//     response.json().then(function(data) {
+//     response.json().then(function (data) {
 //       console.log(data);
 //     });
 //   });
-// }
+// };
 
-// fiveDayForecast();
+// var fiveDayForecast = function (currentCityName) {
+//     // console.log("testing");
+//     // OpenWeather API for 5 Day forecast, formatted to fetch any city
+//     var apiFiveDay =
+//       "http://api.openweathermap.org/data/2.5/forecast?q=" +
+//       currentCityName +
+//       "&appid=24b908f651171bcc6920a65894cdfb4a";
 
-// Use lat/lon from "weather" endpoint to "onecall" API for UV index:
+//     // console.log(apiFiveDay);
+//     fetch(apiFiveDay).then(function (response) {
+//       response.json().then(function (data) {
+//         console.log(data);
+//       });
+//     });
+//   };
 
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
