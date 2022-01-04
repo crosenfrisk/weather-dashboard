0647948a23802dcc9451f58dcf2643545ce94a8f
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-01-03 20:28:57 -0600

    added great API functionality for current city; just need to add UV Index, then 5 day forecast. After that, append search history to buttons on left column

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#textfield");
+
+// When a user types in a city in form input, API generates "Current City" on right side of screen; search also gets added to search history.
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  // get value from input element
+  var currentCityName = cityInputEl.value.trim();
+
+  if (currentCityName) {
+    getWeather(currentCityName);
+    cityInputEl.value = "";
+  } else if (!currentCityName === false) {
+    alert("Please check the spelling of your city and try again.");
+  }
+};
+
+var getWeather = function () {
+  // OpenWeather API formatted to fetch any city
+  var currentCityName = cityInputEl.value.trim();
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    currentCityName +
+    "&units=imperial&appid=24b908f651171bcc6920a65894cdfb4a";
+
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+    //   console.log(data.name);
+    //   console.log(data);
+
+      var divEl = document.querySelector("#Current-City");
+      var currentCityEl = document.querySelector("#update-name");
+      // Take input from user to return current city from API.
+      currentCityEl.textContent = data.name;
+
+     // Take object property "temp" and display to html
+      var tempEl = document.querySelector("#temp");
+      tempEl.textContent = "Temperature: " + data.main.temp + " ° F";
+
+      // Take object property "humidity" and display to html
+      var humidityEl = document.querySelector("#humidity");
+      humidityEl.textContent = "Humidity: " + data.main.humidity + " %";
+
+    // Take object property "wind speed" and display to html
+          var windEl = document.querySelector("#wind");
+          windEl.textContent = "Wind Speed: " + data.wind.speed + " mph";
+
+    // Take object property "icon" and display to html
+      var imgEl = document.createElement("img");
+      imgEl.src =
+        "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+
+      var iconEl = document.querySelector("#icon");
+      icon.textContent = data.weather[0].icon;
+
+      currentCityEl.appendChild(imgEl);
+      currentCityEl.appendChild(iconEl);
+    });
+  });
+};
+
+var fiveDayForecast = function () {
+    // OpenWeather API for 5 Day forcast, formatted to fetch any city
+    var currentCityName = cityInputEl.value.trim();
+    var apiUrl5Day =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      currentCityName +
+      "&units=imperial&appid=24b908f651171bcc6920a65894cdfb4a";
+  
+    fetch(apiUrl5Day).then(function (response) {
+      response.json().then(function (data) {
+        console.log(data);
+      })
+    })
+    }
+
+// Use lat/lon from weather endpoint to Onecall API for UV index.
+
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
